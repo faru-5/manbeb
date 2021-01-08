649be20c7e59d0fc77386784b20b0a45bd7f41cf
Faruq Ismael <farusmith5@gmail.com>
2021-01-08 00:14:38 -0900

    single page book view and some buttons

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.app)
+    path('', views.app, name="app"),
+    path('book/<int:pk>/', views.SingleBook.as_view(), name="singleBook")
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Book
+from django.views.generic import DetailView
 
 def app(request):
     books = Book.objects.all()
     return render(request, 'home/index.html', {"books":books})
+
+def singleBook(request, id):
+    book = get_object_or_404(Book, id=id)
+    return render(request, 'manbeb/single.html', {'id':id})
+
+class SingleBook(DetailView):
+    model = Book
+    template_name = 'manbeb/single.html'
