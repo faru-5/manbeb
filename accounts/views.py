36583305b12ff47cbc53be1e9de5b58b89da54a2
Faruq Ismael <farusmith5@gmail.com>
2021-01-09 10:23:50 -0900

    login and logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
 
-# Create your views here.
+class Login(LoginView):
+    template_name = "auth/login.html"
+    success_url = "/"
+
+    
+class Logout(LogoutView):
+    template_name = "home/about.html"
+
+    
+
